c331e3f7fdd852dec6b15bfd641083a4b6463cbc
Tim Jones <tim@timjones.tw>
2013-01-10 23:46:47 +0800

    Implement basic compiler tests

--- src/MinCamlSharp.Tests/CompilerTests.cs
+++ src/MinCamlSharp.Tests/CompilerTests.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Diagnostics;
+using System.IO;
 using NUnit.Framework;
 
 namespace MinCamlSharp.Tests
@@ -15,19 +16,59 @@ namespace MinCamlSharp.Tests
 
 		private static string[] SourceFiles
 		{
-			get { return Directory.GetFiles("Assets"); }
+			get { return Directory.GetFiles("Assets", "*.ml"); }
 		}
 
 		[TestCaseSource("SourceFiles")]
-		public void CanCompileAndRunApplication(string sourceFile)
+		public void CanCompileApplication(string sourceFile)
+		{
+			// Arrange / Act.
+			var outputFileName = Compile(sourceFile);
+
+			// Assert.
+			Assert.That(File.Exists(outputFileName), Is.True);
+		}
+
+		[TestCaseSource("SourceFiles")]
+		public void CanRunApplication(string sourceFile)
+		{
+			// Arrange.
+			var outputFileName = Compile(sourceFile);
+			string referenceFileName = Path.ChangeExtension(sourceFile, ".ref");
+			if (File.Exists(referenceFileName))
+				Assert.Inconclusive("Could not complete test: no reference file provided.");
+			var referenceFileContents = File.ReadAllText(referenceFileName);
+
+			// Act.
+			string actualOutput = null;
+			var process = new Process
+			{
+				StartInfo =
+				{
+					FileName = outputFileName,
+					UseShellExecute = false,
+					RedirectStandardOutput = true
+				}
+			};
+			process.OutputDataReceived += (sender, args) => actualOutput = args.Data;
+			process.Start();
+			process.BeginOutputReadLine();
+			process.WaitForExit();
+
+			// Assert.
+			Assert.That(actualOutput, Is.EqualTo(referenceFileContents));
+		}
+
+		private string Compile(string sourceFile)
 		{
 			// Arrange.
 			var compiler = new Compiler();
+			string outputFileName = Path.Combine("Binaries", Path.GetFileNameWithoutExtension(sourceFile) + ".exe");
 			var options = new CompilerOptions
 			{
 				SourceFile = sourceFile,
 				OutputAssemblyName = "Test",
-				OutputFileName = Path.Combine("Binaries", Path.GetFileNameWithoutExtension(sourceFile) + ".exe")
+				OutputFileName = outputFileName
 			};
 
 			// Act.
@@ -35,6 +76,8 @@ namespace MinCamlSharp.Tests
 
 			// Assert.
 			Assert.That(result, Is.True);
+
+			return outputFileName;
 		}
 	}
 }
