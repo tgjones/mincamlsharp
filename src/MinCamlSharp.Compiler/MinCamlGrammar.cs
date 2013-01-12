e4d82c7ee08a9e8c5b48f0f85a496868260e7d11
Tim Jones <tim@timjones.tw>
2013-01-12 23:39:48 +0800

    Initial work on Irony-based version

---
+++ src/MinCamlSharp.Compiler/MinCamlGrammar.cs
@@ -0,0 +1,13 @@
+﻿using Irony.Parsing;
+
+namespace MinCamlSharp.Compiler
+{
+	[Language("MinCaml", "0.1", "Minimal subset of Objective Caml")]
+	public class MinCamlGrammar : Grammar
+	{
+		 public MinCamlGrammar()
+		 {
+			 
+		 }
+	}
+}
