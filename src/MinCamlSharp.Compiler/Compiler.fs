b8ee097e03b6781faabae7018dd0f4a67b8f2566
Tim Jones <tim@timjones.tw>
2013-01-15 23:00:21 +0800

    Implement optimisation

--- src/MinCamlSharp.Compiler/Compiler.fs
+++ src/MinCamlSharp.Compiler/Compiler.fs
@@ -5,14 +5,19 @@ open System.IO
 type Compiler(options : CompilerOptions) =
     let assemblyGenerator = new AssemblyGenerator(options)
 
-//    let limit = ref 1000
-//
-//    let rec iter n e =
-//        Printf.eprintf "iteration %d@." n;
-//        if n = 0 then e else
-//        let e' = Elim.transform (ConstFold.f (Inline.f (Assoc.f (Beta.f e)))) in
-//        if e = e' then e else
-//        iter (n - 1) e'
+    let limit = ref 1000
+
+    let rec iter n e =
+        Printf.eprintf "iteration %d@." n;
+        if n = 0 then e else
+        let e' =
+            Beta.transform e
+                |> Assoc.transform
+                |> Inline.transform
+                |> ConstFold.transform
+                |> Elim.transform in
+        if e = e' then e else
+        iter (n - 1) e'
         
     member x.Compile() =
         Lexer.CreateBufferFromString(File.ReadAllText(options.SourceFile))
@@ -20,5 +25,6 @@ type Compiler(options : CompilerOptions) =
             |> Typing.transform
             |> KNormal.transform
             |> Alpha.transform
+            |> iter !limit
             |> assemblyGenerator.GenerateAssembly
         true
