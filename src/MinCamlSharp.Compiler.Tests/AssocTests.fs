53cecc236d8c5752802bcaf8e8d6c17469faca11
Tim Jones <tim@timjones.tw>
2013-01-15 22:43:53 +0800

    Implement reduction of nested lets

---
+++ src/MinCamlSharp.Compiler.Tests/AssocTests.fs
@@ -0,0 +1,86 @@
+﻿module MinCamlSharp.Compiler.Tests.Assoc
+
+open MinCamlSharp.Compiler
+open MinCamlSharp.Compiler.KNormal
+open NUnit.Framework
+open NUnit.Framework.Constraints
+
+[<Test>]
+let TestReduceNestedLets() =
+    // Arrange.
+    let input = 
+        LetRec (
+          { name = ("sum.1", Type.Fun([Type.Int], Type.Int));
+            args = [("x.2", Type.Int)];
+            body =
+                Let(
+                    ("Ti3.3", Type.Int),
+                    Int(0),
+                    IfLE(
+                        "x.2", "Ti3.3", Int(0),
+                        Let(
+                            ("Ti6.4", Type.Int),
+                            Let(
+                                ("Ti5.5", Type.Int),
+                                Let(
+                                    ("Ti4.6", Type.Int),
+                                    Int(1),
+                                    Sub("x.2", "Ti4.6")
+                                ),
+                                App("sum.1", ["Ti5.5"])
+                            ),
+                            Add("Ti6.4", "x.2")
+                        )
+                    )
+                )},
+            Let(
+                ("Ti2.7", Type.Int),
+                Let(
+                    ("Ti1.8", Type.Int),
+                    Int(10000),
+                    App("sum.1", ["Ti1.8"])
+                ),
+                ExtFunApp("print_int", ["Ti2.7"])
+            )
+        )
+
+    // Act.
+    let output = Assoc.transform input
+
+    // Assert.
+    let expectedOutput = 
+        LetRec (
+          { name = ("sum.1", Type.Fun([Type.Int], Type.Int));
+            args = [("x.2", Type.Int)];
+            body =
+                Let(
+                    ("Ti3.3", Type.Int),
+                    Int(0),
+                    IfLE(
+                        "x.2", "Ti3.3", Int(0),
+                        Let(
+                            ("Ti4.6", Type.Int),
+                            Int(1),
+                            Let(
+                                ("Ti5.5", Type.Int),
+                                Sub("x.2", "Ti4.6"), 
+                                Let(
+                                    ("Ti6.4", Type.Int),
+                                    App("sum.1", ["Ti5.5"]),
+                                    Add("Ti6.4", "x.2")
+                                )
+                            )
+                        )
+                    )
+                )},
+            Let(
+                ("Ti1.8", Type.Int),
+                Int(10000),
+                Let(
+                    ("Ti2.7", Type.Int),
+                    App("sum.1", ["Ti1.8"]),
+                    ExtFunApp("print_int", ["Ti2.7"])
+                )
+            )
+        )
+    Assert.That(output, Is.EqualTo(expectedOutput))
---
+++ src/MinCamlSharp.Compiler/Assoc.fs
@@ -0,0 +1,20 @@
+﻿module MinCamlSharp.Compiler.Assoc
+
+(* flatten let-bindings (just for prettier printing) *)
+
+open KNormal
+
+let rec transform = function
+    | IfEq(x, y, e1, e2) -> IfEq(x, y, transform e1, transform e2)
+    | IfLE(x, y, e1, e2) -> IfLE(x, y, transform e1, transform e2)
+    | Let(xt, e1, e2) ->
+        let rec insert = function
+            | Let(yt, e3, e4) -> Let(yt, e3, insert e4)
+            | LetRec(fundefs, e) -> LetRec(fundefs, insert e)
+            | LetTuple(yts, z, e) -> LetTuple(yts, z, insert e)
+            | e -> Let(xt, e, transform e2) in
+                insert (transform e1)
+    | LetRec({ name = xt; args = yts; body = e1 }, e2) ->
+        LetRec({ name = xt; args = yts; body = transform e1 }, transform e2)
+    | LetTuple(xts, y, e) -> LetTuple(xts, y, transform e)
+    | e -> e
